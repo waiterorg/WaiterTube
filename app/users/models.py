ee5081089daea8c0ea2d677c67b3e9580d434eb0
waiterorg <iwaiterorg@gmail.com>
2022-06-19 23:40:20 +0430

    Create user model

---
+++ app/users/models.py
@@ -0,0 +1,18 @@
+import uuid
+from app.config import get_settings
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import Model
+
+settings = get_settings()
+
+class User(Model):
+    __keyspace__ = settings.keyspace
+    email = columns.Text(primary_key=True)
+    user_id = columns.UUID(primary_key=True, default=uuid.uuid1) # UUID1
+    password = columns.Text() # secure
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f"User(email={self.email}, user_id={self.user_id})"
